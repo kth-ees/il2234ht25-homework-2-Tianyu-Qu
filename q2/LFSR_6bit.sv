b01b05cc8852e58906395d24e298d14a92ccf27f
屈添雨（Qu Tianyu） <tianyu.qu@studio.unibo.it>
2025-09-20 22:37:15 +0200

    Update LFSR_6bit.sv

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -4,7 +4,21 @@ module LFSR_6bit (
   input  logic [5:0] parallel_in,
   output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+  logic [5:0] q;
+
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      q <= '0;
+    end
+    else if (sel == 1'b0) begin
+      q <= parallel_in;
+    end
+    else begin
+      q <= { (q[5] ^ q[4]), q[5:1] };
+    end
+  end
+
+  assign parallel_out = q;
+
 endmodule
+
