9195553864b146d650cbcf56a62dcd6e0eb3e860
屈添雨（Qu Tianyu） <tianyu.qu@studio.unibo.it>
2025-09-20 20:37:14 +0200

    Update shift_register_tb.sv

--- q1/shift_register_tb.sv
+++ q1/shift_register_tb.sv
@@ -1,5 +1,46 @@
 module shift_register_tb;
 
-// complete here
+localparam int N = 4;
+
+logic clk; 
+logic rst_n; 
+logic serial_parallel; 
+logic load_enable; 
+logic serial_in; 
+logic [N-1:0] parallel_in; 
+logic [N-1:0] parallel_out;
+logic serial_out; 
+
+
+
+shift_register #(.N(N)) LUT(
+.clk(clk),
+.rst_n(rst_n), 
+.serial_parallel(serial_parallel), 
+.load_enable(load_enable),
+.serial_in(serial_in),
+.parallel_in(parallel_in), 
+.parallel_out(parallel_out),
+.serial_out(serial_out) 
+); 
+
+
+initial begin
+clk = 1'b0;
+forever #5 clk =~clk;
+end
+
+initial begin
+rst_n = 1'b0;
+serial_parallel = 1'b0;
+load_enable = 1'b0;
+serial_in = 1'b0;
+parallel_in = 4'b0;
+
+#5 rst_n = 1'b1;
+#5 load_enable = 1'b1; serial_in = 1'b1;
+#40 serial_parallel = 1'b1; parallel_in = 4'b0;
+#20 $stop;
+end
 
 endmodule
