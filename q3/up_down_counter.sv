541b8334c4185dde1c6c831cb745cd38a0c2a3e2
屈添雨（Qu Tianyu） <tianyu.qu@studio.unibo.it>
2025-09-20 22:06:49 +0200

    Update up_down_counter.sv

--- q3/up_down_counter.sv
+++ q3/up_down_counter.sv
@@ -7,6 +7,43 @@ module up_down_counter #(parameter N = 4)
                         output logic [N-1:0] count_out,
                         output logic carry_out);
   
-  // complete here
+logic [N-1:0] Q;
+localparam logic [N-1:0] MAX = {N{1'b1}};
+
+always_ff @(posedge clk or negedge rst_n)begin
+if(!rst_n)begin
+	Q <= '0;
+      	carry_out <= 1'b0;
+end
+else if(load)begin
+	Q <= input_load;
+	carry_out <= 1'b0;
+	end 
+else if(up_down) begin
+	if(Q == MAX)begin
+		Q <= '0;
+		carry_out <= 1'b1;
+	end
+	else begin
+		Q <= Q + 1'b1;
+		carry_out <= 1'b0;
+	end	
+end	
+else begin
+	if(Q == '0)begin
+		Q <= MAX;
+		carry_out <= 1'b1;
+	end
+	else begin
+		Q <= Q - 1'b1;
+		carry_out <= 1'b0;
+	end	
+end	
+
+end
+
+assign count_out = Q;
+
+
 
 endmodule
