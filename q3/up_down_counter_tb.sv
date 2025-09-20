130f7302f0cf24b91479c8a3b06f03da474014ff
屈添雨（Qu Tianyu） <tianyu.qu@studio.unibo.it>
2025-09-20 22:07:29 +0200

    Update up_down_counter_tb.sv

--- q3/up_down_counter_tb.sv
+++ q3/up_down_counter_tb.sv
@@ -1,5 +1,47 @@
 module up_down_counter_tb;
+  
+localparam int N = 4;
 
-// complete here
+logic clk;
+logic rst_n;
+logic up_down;
+logic load;
+logic [N-1:0] input_load;
+logic [N-1:0] count_out;
+logic carry_out;
+
+
+
+up_down_counter #(.N(N)) LUT(
+.clk(clk),
+.rst_n(rst_n), 
+.up_down(up_down), 
+.load(load),
+.input_load(input_load),
+.count_out(count_out), 
+.carry_out(carry_out)
+); 
+
+
+initial begin
+clk = 1'b0;
+forever #5 clk =~clk;
+end
+
+initial begin
+rst_n = 1'b0;
+up_down = 1'b0;
+load = 1'b0;
+input_load = 1'b0;
+
+#10 rst_n = 1'b1;
+#10 load = 1'b1; input_load = 4'b0111;
+#10 load = 1'b0; up_down = 1'b1;
+#110 load = 1'b1;
+#10 load = 1'b0; up_down = 1'b0;
+#20 $stop;
+end
+
+
 
 endmodule
